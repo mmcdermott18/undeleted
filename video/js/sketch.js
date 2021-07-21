aca2a8afcc1eeebcf266a0bafd17d213cd39730d
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2021-07-21 11:08:32 -0400

    fixed some things + buttons

--- video/js/sketch.js
+++ video/js/sketch.js
@@ -5,15 +5,20 @@ let images = [];
 let imgAmount = 6; // set number of images in folder
 let imgIndex = 0;
 let c = 0;
-let fr;
-let frMin = 5;
-let frMax = 30;
+let fr; //amount of frames image stays on screen
+let r; //current shuffler value
+let rMax = 4; //high value for shuffler
+let frLMin = 1; //frame rate minumum
+let frLMax = 5; //frame rate minumum
+let frHMin = 30; //high value frame rate minumum
+let frHMax = 90; //high value frame rate maximum
 
 let counter = 0;
 let length = 10; // in seconds
 let end = 30 * length; // in frames
 
 var capturer = new CCapture({ format: 'webm' , framerate: 30 , name: length + 'sec-' + fName });
+var started = true;
 
 function preload() {
   for (let i = 0; i < imgAmount; i++) {
@@ -26,32 +31,55 @@ function preload() {
 function setup() {
   frameRate(30);
   createCanvas(windowHeight / 1.7777777, windowHeight);
-  fr = int(random(frMin, frMax));
-  background(0);
+  r = int(random(0, rMax + 1));
+  if (r == rMax) {
+    fr = int(random(frHMin, frHMax));
+  } else {
+    fr = int(random(frLMin, frLMax));
+  };
+  background(255);
   capturer.start();
 }
 
 function draw() {
-  console.log("counter");
-  if (counter == end) {
-    counter = 0;
-    save_record();
-    setTimeout(function(){startover()},100);
-  } else {
-    counter++;
+  click();
+  if (started) {
+    console.log("counter");
+    if (counter == end) {
+      counter = 0;
+      save_record();
+      setTimeout(function(){startover()},100);
+    } else {
+      counter++;
+    }
+    clear();
+    img = images[imgIndex];
+    if (c < fr) {
+      imageMode(CORNER);
+      image(img, 0, (height - img.height*width/img.width) / 2, width, img.height*width/img.width); // to fit width
+      c++;
+    } else {
+      // regenerate r and fr
+      r = int(random(0, rMax + 1));
+      if (r == rMax) {
+        fr = int(random(frHMin, frHMax));
+      } else {
+        fr = int(random(frLMin, frLMax));
+      };
+      imageMode(CORNER);
+      image(img, 0, (height - img.height*width/img.width) / 2, width, img.height*width/img.width); // to fit width
+      moveFrame();
+      c = 0;
+    }
+    capturer.capture(document.getElementById('defaultCanvas0'));
   }
-  clear();
-  img = images[imgIndex];
-  if (c < fr) {
-    imageMode(CORNER);
-    image(img, 0, (height - img.height*width/img.width) / 2, width, img.height*width/img.width); // to fit width
-    c++;
-  } else {
-    fr = int(random(frMin, frMax)); //regenerate
-    moveFrame();
-    c = 0;
-  }
-  capturer.capture(document.getElementById('defaultCanvas0'));
+  document.querySelector('#stop').addEventListener("click", function () {
+    started = false;
+    draw();
+  });
+  document.querySelector('#reload').addEventListener("click", function () {
+    startover();
+  });
 }
 
 function moveFrame() {
@@ -64,6 +92,14 @@ function moveFrame() {
 function save_record() {
   capturer.save();
 }
+
 function startover() {
   window.location.reload();
+}
+
+function click() {
+  document.querySelector('#start').addEventListener("click", function () {
+    started = true;
+    draw();
+  });
 }
